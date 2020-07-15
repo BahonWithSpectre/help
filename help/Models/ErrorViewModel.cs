6b494edcffbcff3bd47b4a26174253fd2dd20c36
bakytzhan.shymkentay <bakytzhan.shymkentay@DESKTOP-RM08NSD>
2020-07-15 14:46:55 +0300

    Добавьте файлы проекта.

---
+++ help/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace help.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
